d80c4c53f6aeade82364b99748d50d243d67a7ff
asfarmunir <asfarmunir12345@gmail.com>
2024-07-27 01:27:26 +0500

    ui update

--- app/page.tsx
+++ app/page.tsx
@@ -124,7 +124,7 @@ export default function Home() {
             className=" absolute -right-3 -bottom-3 -rotate-4 "
           />
         </h1>
-        <div className="flex items-end my-16 2xl:my-20">
+        <div className="hidden lg:flex items-end my-16 2xl:my-20">
           <div className="w-[10.5rem] 2xl:w-[11.8rem] h-[6.5rem] 2xl:h-[7.5rem] bg-[#F3F3F9] flex py-2 items-center rounded-tr-3xl rounded-tl-3xl rounded-bl-3xl  justify-center">
             <div className="flex bg-white rounded-2xl w-36  2xl:w-[10.4rem] h-[5.2rem] 2xl:h-[5.6rem] flex-col relative  items-center justify-center">
               <Image
@@ -226,12 +226,12 @@ export default function Home() {
         </div>
       </section>
       {/* Partners */}
-      <section className="flex flex-col  items-center gap-5 2xl:mt-16 mb-16 md:mb-10">
+      <section className="flex flex-col  w-full items-center gap-5 2xl:mt-16 mb-16 md:mb-10">
         <h2 className=" text-base   2xl:xl tracking-wide ">
           What do we specialize in?
         </h2>
         <h1
-          className={` text-xl relative md:text-3xl 2xl:text-5xl tracking-wide text-center ${recoleta.className}`}
+          className={` text-xl relative md:text-3xl 2xl:text-5xl tracking-wide text-center mb-16 ${recoleta.className}`}
         >
           Our partners hire us to{" "}
           <span className=" text-[#7165FF] ">design & develop</span> their…
@@ -243,6 +243,128 @@ export default function Home() {
             className=" absolute right-36 -bottom- -rotate-4 "
           />
         </h1>
+        <div className="grid grid-cols-1 max-w-7xl lg:grid-cols-2 gap-8 px-8 md:px-12 2xl:px-0  p-2 w-full">
+          <div className="flex flex-col md:flex-row w-full  overflow-hidden  rounded-2xl ">
+            <div className=" py-10 md:py-0 w-full md:w-[50%] bg-[#F3F3F9]  flex flex-col items-center justify-center">
+              <h2 className=" text-2xl md:text-lg 2xl:text-2xl font-semibold mb-2">
+                Mobile & Web Apps
+              </h2>
+              <p className=" md:max-w-[15.5rem] 2xl:max-w-[20rem] px-8 text-lg md:text-xs 2xl:text-sm text-center md:text-start ">
+                We’ll research your product and start building.
+              </p>
+            </div>
+            <div className=" w-full md:w-[50%] bg-[#F3F3F9] ">
+              <Image
+                src="/images/portfolio1.jpg"
+                alt="Splenify"
+                width={225}
+                objectFit="cover"
+                className=" w-full h-full "
+                height={123}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row w-full  overflow-hidden  rounded-2xl ">
+            <div className=" py-10 md:py-0 w-full md:w-[50%] bg-[#F3F3F9]  flex flex-col items-center justify-center">
+              <h2 className=" text-2xl md:text-lg 2xl:text-2xl font-semibold mb-2">
+                Website
+              </h2>
+              <p className=" md:max-w-[15.5rem] 2xl:max-w-[20rem] px-8 text-lg md:text-xs 2xl:text-sm text-center md:text-start ">
+                We’ll research your product and start building.
+              </p>
+            </div>
+            <div className=" w-full md:w-[50%] bg-[#F3F3F9] ">
+              <Image
+                src="/images/portfolio2.jpg"
+                alt="Splenify"
+                width={225}
+                objectFit="cover"
+                className=" w-full h-full "
+                height={123}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row w-full  overflow-hidden  rounded-2xl ">
+            <div className=" py-10 md:py-0 w-full md:w-[50%] bg-[#F3F3F9]  flex flex-col items-center justify-center">
+              <h2 className=" text-2xl md:text-lg 2xl:text-2xl font-semibold mb-2">
+                Mobile & Web Apps
+              </h2>
+              <p className=" md:max-w-[15.5rem] 2xl:max-w-[20rem] px-8 text-lg md:text-xs 2xl:text-sm text-center md:text-start ">
+                We’ll research your product and start building.
+              </p>
+            </div>
+            <div className=" w-full md:w-[50%] bg-[#F3F3F9] ">
+              <Image
+                src="/images/portfolio1.jpg"
+                alt="Splenify"
+                width={225}
+                objectFit="cover"
+                className=" w-full h-full "
+                height={123}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row w-full  overflow-hidden  rounded-2xl ">
+            <div className=" py-10 md:py-0 w-full md:w-[50%] bg-[#F3F3F9]  flex flex-col items-center justify-center">
+              <h2 className=" text-2xl md:text-lg 2xl:text-2xl font-semibold mb-2">
+                Website
+              </h2>
+              <p className=" md:max-w-[15.5rem] 2xl:max-w-[20rem] px-8 text-lg md:text-xs 2xl:text-sm text-center md:text-start ">
+                We’ll research your product and start building.
+              </p>
+            </div>
+            <div className=" w-full md:w-[50%] bg-[#F3F3F9] ">
+              <Image
+                src="/images/portfolio2.jpg"
+                alt="Splenify"
+                width={225}
+                objectFit="cover"
+                className=" w-full h-full "
+                height={123}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row w-full  overflow-hidden  rounded-2xl ">
+            <div className=" py-10 md:py-0 w-full md:w-[50%] bg-[#F3F3F9]  flex flex-col items-center justify-center">
+              <h2 className=" text-2xl md:text-lg 2xl:text-2xl font-semibold mb-2">
+                Mobile & Web Apps
+              </h2>
+              <p className=" md:max-w-[15.5rem] 2xl:max-w-[20rem] px-8 text-lg md:text-xs 2xl:text-sm text-center md:text-start ">
+                We’ll research your product and start building.
+              </p>
+            </div>
+            <div className=" w-full md:w-[50%] bg-[#F3F3F9] ">
+              <Image
+                src="/images/portfolio1.jpg"
+                alt="Splenify"
+                width={225}
+                objectFit="cover"
+                className=" w-full h-full "
+                height={123}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row w-full  overflow-hidden  rounded-2xl ">
+            <div className=" py-10 md:py-0 w-full md:w-[50%] bg-[#F3F3F9]  flex flex-col items-center justify-center">
+              <h2 className=" text-2xl md:text-lg 2xl:text-2xl font-semibold mb-2">
+                Website
+              </h2>
+              <p className=" md:max-w-[15.5rem] 2xl:max-w-[20rem] px-8 text-lg md:text-xs 2xl:text-sm text-center md:text-start ">
+                We’ll research your product and start building.
+              </p>
+            </div>
+            <div className=" w-full md:w-[50%] bg-[#F3F3F9] ">
+              <Image
+                src="/images/portfolio2.jpg"
+                alt="Splenify"
+                width={225}
+                objectFit="cover"
+                className=" w-full h-full "
+                height={123}
+              />
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   );
